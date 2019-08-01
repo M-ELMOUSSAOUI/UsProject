10763deb23f408113178030560308c2e4ee7eb65
moussa23 <mosawi.man@live.fr>
2019-08-01 21:00:05 +0100

    Initial commit

--- app/src/main/java/com/zizi/mouad/apprentissageacademy/MainActivity.java
+++ app/src/main/java/com/zizi/mouad/apprentissageacademy/MainActivity.java
@@ -1,90 +1,132 @@
 package com.zizi.mouad.apprentissageacademy;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     // Declaration of Variables
-
     private Button register, SignIn;
     private EditText Email, Password;
     private TextView PrivacyPolicy;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private FirebaseAuth mAuth;
 
     // Main function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
 
         //Instance Objects
-        register = findViewById( R.id.Sinscrire );
-        SignIn = findViewById( R.id.SeConnect );
-        Email = findViewById( R.id.EmailSeConnect );
-        Password = findViewById( R.id.PasswordSeconnect );
-        PrivacyPolicy = findViewById( R.id.privacyPolicy01 );
+        register = findViewById(R.id.Sinscrire);
+        SignIn = findViewById(R.id.SeConnect);
+        Email = findViewById(R.id.EmailSeConnect);
+        Password = findViewById(R.id.PasswordSeconnect);
+        PrivacyPolicy = findViewById(R.id.privacyPolicy01);
 
         // Fonction On Click
 
-        register.setOnClickListener( new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity( new Intent( MainActivity.this, sinscrire.class ) );
+                startActivity(new Intent(MainActivity.this, sinscrire.class));
             }
-        } );
+        });
 
-        SignIn.setOnClickListener( new View.OnClickListener() {
+        SignIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 UserLogIn();
             }
-        } );
+        });
 
-        PrivacyPolicy.setOnClickListener( new View.OnClickListener() {
+        PrivacyPolicy.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity( new Intent( MainActivity.this, PrivacyPolicy.class ) );
+                startActivity(new Intent(MainActivity.this, PrivacyPolicy.class));
             }
-        } );
+        });
 
     }
-        // Error Messages
+    // Error Messages
 
-            public void UserLogIn(){
+    public void UserLogIn() {
 
-            String stringEmail = Email.getText().toString().trim();
-            String stringPassword = Password.getText().toString().trim();
+        String stringEmail = Email.getText().toString().trim();
+        String stringPassword = Password.getText().toString().trim();
 
-            if (stringEmail.isEmpty()) {
-                Email.setError( "Entrez votre email" );
-                Email.requestFocus();
-                return;
-            }
-            if (stringPassword.isEmpty()) {
-                Password.setError( "Entrez votre mot de pass" );
-                Password.requestFocus();
-                return;
-            }
+        if (stringEmail.isEmpty()) {
+            Email.setError("Entrez votre email");
+            Email.requestFocus();
+            return;
+        }
+        if (stringPassword.isEmpty()) {
+            Password.setError("Entrez votre mot de pass");
+            Password.requestFocus();
+            return;
+        }
 
-            if (!stringEmail.matches(emailPattern)) {
-                Email.setError( "Email Invalid" );
-                Email.requestFocus();
-                return;
-            }
+        if (!stringEmail.matches(emailPattern)) {
+            Email.setError("Email Invalid");
+            Email.requestFocus();
+            return;
+        }
 
-            if (stringPassword.length()<8) {
-                Password.setError( "Mot de passe doit comporter au moins 8 caractères" );
-                Password.requestFocus();
-                return;
-            }
-                startActivity( new Intent( MainActivity.this, Index.class ) );
+        if (stringPassword.length() < 8) {
+            Password.setError("Mot de passe doit comporter au moins 8 caractères");
+            Password.requestFocus();
+            return;
+        }
+        //Sign in the user
+        mAuth.signInWithEmailAndPassword(stringEmail, stringPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(MainActivity.this, Index.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
 
+                            Toast.makeText(MainActivity.this, "Email ou mot de pass incorrect",
+                                    Toast.LENGTH_SHORT).show();
 
-            }
+                        }
+                    }
+                });
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(MainActivity.this, Index.class);
+            startActivity(intent);
+        }
+    }
+
+}
+
+
+
+
 
 
