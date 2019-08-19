ae88b2d90b74b98e83584386f2700a314ae3b96c
moussa23 <mosawi.man@live.fr>
2019-08-19 17:20:47 +0100

    Initial commit

--- app/src/main/java/com/zizi/mouad/apprentissageacademy/AFragments/VideoCollege1.java
+++ app/src/main/java/com/zizi/mouad/apprentissageacademy/AFragments/VideoCollege1.java
@@ -2,6 +2,7 @@ package com.zizi.mouad.apprentissageacademy.AFragments;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,8 +13,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
+import com.zizi.mouad.apprentissageacademy.ActivityVideoView;
 import com.zizi.mouad.apprentissageacademy.CustomDialog;
 import com.zizi.mouad.apprentissageacademy.R;
 
@@ -22,6 +27,8 @@ import com.zizi.mouad.apprentissageacademy.R;
  */
 public class VideoCollege1 extends Fragment  {
     private static final String TAG = "VideoCollege1";
+    TextView go;
+
 
 
     public VideoCollege1() {
@@ -34,7 +41,13 @@ public class VideoCollege1 extends Fragment  {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =inflater.inflate(R.layout.fragment_video_college1, container, false);
-
+        go =v.findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ActivityVideoView.class));
+            }
+        });
 
 
         return v;
